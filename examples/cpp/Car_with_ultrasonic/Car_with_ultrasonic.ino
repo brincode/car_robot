4a8615b1410b581b192036b5a4ba20746214e5e7
brin <142337149+brincode@users.noreply.github.com>
2024-04-28 00:19:35 +0300

    l

--- examples/cpp/Car_with_ultrasonic/Car_with_ultrasonic.ino
+++ examples/cpp/Car_with_ultrasonic/Car_with_ultrasonic.ino
@@ -1,29 +1,32 @@
-// First Include the NewPing and Servo Libraries 
+
+                                         
+ // First Include the NewPing and Servo Libraries 
 
 #include <NewPing.h> // library uses to read more accurate values from ultrasonic sensor 
 #include <Servo.h> 
 
-#define TRIG_PIN A1 
-#define ECHO_PIN A2
+#define TRIG_PIN A4 
+#define ECHO_PIN A5 
 #define MAX_DISTANCE 200 
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); // sensor function 
 Servo myservo;   
 
 int distance = 100;
+
 // Motor A
-const int motorPin1  = 6;  //left motor 1st pin  
-const int motorPin2  = 5;  //left motor 2st pin
+const int motorPin1  = 11;  
+const int motorPin2  = 10;  
 
 //Motor B
-const int motorPin3  = 11; //right motor 1st pin
-const int motorPin4  = 10;  //right motor 2st pin
+const int motorPin3  = 6; 
+const int motorPin4  = 5;  
 
 void setup() {  
   
-  myservo.attach(8);  // pin of servo motor
-  myservo.write(115); 
+  myservo.attach(9);  // pin of servo motor
+  myservo.write(110); 
   delay(2000);
-  distance = readPing(); 
+  distance = readPing(); // clears the trigpin
   delay(100);
   distance = readPing();
   delay(100);
@@ -45,7 +48,7 @@ void loop() {
   moveStop();
   delay(100);
   moveBackward();
-  delay(400);
+  delay(300);
   moveStop();
   delay(200);
   distanceR = lookRight();
@@ -56,15 +59,16 @@ void loop() {
   if(distanceR>=distanceL)
     {
       turnRight();
-    
+      moveStop();
     }
   else
     {
       turnLeft();
+      moveStop();
     }
  }
  else
-  { 
+  {
     moveForward();
   }
  
@@ -104,47 +108,47 @@ int readPing() {  // defines a function returns an integer
 }
 
 void moveStop() {
-  analogWrite(motorPin1, 1);
-    analogWrite(motorPin2, 1);
-    analogWrite(motorPin3, 1);
-    analogWrite(motorPin4, 1);
+  analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin4, 0);
   } 
   
 void moveForward() {
 
 
-    analogWrite(motorPin1, 0);
-    analogWrite(motorPin2, 180);
-    analogWrite(motorPin3, 180);
-    analogWrite(motorPin4, 0);  
+    analogWrite(motorPin1, 180);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin4, 180);  
   
 }
 
 void moveBackward() {
     
-    analogWrite(motorPin1, 180);
-    analogWrite(motorPin2, 0);
+    analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 180);
     analogWrite(motorPin3, 180);
     analogWrite(motorPin4, 0);   
   
 }  
 
 void turnRight() {
- analogWrite(motorPin1, 0);
+ analogWrite(motorPin1, 180);
     analogWrite(motorPin2, 0);
   analogWrite(motorPin3, 0);
     analogWrite(motorPin4, 180);    
-  delay(1000);
+  delay(300);
  moveForward();      
   
 } 
  
 void turnLeft() {
-  analogWrite(motorPin1, 180);
-    analogWrite(motorPin2, 0);    
- analogWrite(motorPin3, 0);
+  analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 180);   
+ analogWrite(motorPin3, 180);
     analogWrite(motorPin4, 0);     
-  delay(1000);
+  delay(300);
    moveForward();
 }  
 
